a75a961642e01c96dcba41c8b7881371ccb18a50
Matthew Connell-Tombs <mattct16@yahoo.ca>
2023-09-04 10:52:28 -0400

    Fixed js animation for 1/2 images, and indented code

--- skills.js
+++ skills.js
@@ -7,10 +7,28 @@ const images = document.querySelectorAll(".carousel-icon-container");
 
 const totalImages = Object.keys(images).length;
 
+function calculateCardWidth() {
+    let imagesDisplayed;
+
+    if (window.innerWidth <= 750) {
+        imagesDisplayed = 1;
+    } else if (window.innerWidth <= 1440) {
+        imagesDisplayed = 2;
+    } else {
+        imagesDisplayed = 3;
+    }
+    const carouselContainer = document.querySelector(".carousel-container");
+    const containerWidth = carouselContainer.offsetWidth;
+
+    // Calculate the width of each card
+    return (containerWidth - 20 * (imagesDisplayed - 1)) / imagesDisplayed;
+}
+
 let imageWidth = getElementWidth();
 
 function getElementWidth() {
-    return images[1].getBoundingClientRect().width + 20;
+    let card_width = calculateCardWidth()
+    return card_width + 20;
 }
 
 function handleResize() {
@@ -22,50 +40,50 @@ window.addEventListener('resize', handleResize);
 let animationInProgress = false;
 
 function handleArrowClick(direction) {
-  if (animationInProgress) {
-    return;
-  }
+    if (animationInProgress) {
+        return;
+    }
+    console.log(calculateCardWidth())
+    animationInProgress = true;
 
-  animationInProgress = true;
+    if (direction === "left") {
 
-  if (direction === "left") {
+        prevIndex = currentIndex;
+        currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+        carousel.style.transform = `translateX(-${imageWidth}px)`;
+        carousel.insertBefore(images[currentIndex].parentNode, carousel.firstChild);
 
-    prevIndex = currentIndex;
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-    carousel.style.transform = `translateX(-${imageWidth}px)`;
-    carousel.insertBefore(images[currentIndex].parentNode, carousel.firstChild);
+        setTimeout(() => {
+            carousel.style.transform = "";
+            carousel.classList.add("sliding-transition");
+        }, 10);
 
-    setTimeout(() => {
-        carousel.style.transform = "";
+        setTimeout(() => {
+            carousel.classList.remove("sliding-transition");
+            animationInProgress = false;
+        }, 500);
+
+    } else if (direction === "right") {
         carousel.classList.add("sliding-transition");
-    }, 10);
 
-    setTimeout(() => {
-        carousel.classList.remove("sliding-transition");
-        animationInProgress = false;
-    }, 500);
+        prevIndex = currentIndex;
+        currentIndex = (currentIndex + 1) % totalImages;
 
-  } else if (direction === "right") {
-    carousel.classList.add("sliding-transition");
+        carousel.style.transform = `translateX(-${imageWidth}px)`;
 
-    prevIndex = currentIndex;
-    currentIndex = (currentIndex + 1) % totalImages;
-
-    carousel.style.transform = `translateX(-${imageWidth}px)`;
-
-    setTimeout(() => {
-        carousel.appendChild(images[prevIndex].parentNode);
-        carousel.classList.remove("sliding-transition");
-        carousel.style.transform = "";
-        animationInProgress = false;
-    }, 500);
-  }
+        setTimeout(() => {
+            carousel.appendChild(images[prevIndex].parentNode);
+            carousel.classList.remove("sliding-transition");
+            carousel.style.transform = "";
+            animationInProgress = false;
+        }, 500);
+    }
 }
 
 leftArrow.addEventListener("click", () => {
-  handleArrowClick("left");
+    handleArrowClick("left");
 });
 
 rightArrow.addEventListener("click", () => {
-  handleArrowClick("right");
+    handleArrowClick("right");
 });
